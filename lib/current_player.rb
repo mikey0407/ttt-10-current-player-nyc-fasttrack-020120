af30896dc7432acd7da3c93e14500254e3cb3ac7
Michael Alfaro <michael.s.alfaro@gmail.com>
2020-02-07 22:28:10 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+
+def turn_count(board)
+counter = 0
+board.each do |cp|
+if cp.downcase == "x" || cp.downcase == "o" 
+  counter += 1 
+end
+end
+  return counter
+end
+
+def current_player(board)
+  turn_count(board)
+  return counter
+  if #{counter} % 2 = 0
+  puts "It's X turn"
+else 
+  
